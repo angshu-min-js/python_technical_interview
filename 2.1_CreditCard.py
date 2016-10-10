cf38dfc945076c4dff5f067ba11a3c43fd4a2ca5
angshu-min-js <angshu.gupta789@gmail.com>
2016-10-10 19:18:53 +0530

    removed errors

--- 2.1_CreditCard.py
+++ 2.1_CreditCard.py
@@ -1,52 +1,52 @@
 class CreditCard:
-	 ”””A consumer credit card.”””
+	 '''A consumer credit card.'''
 
-	 def init (self, customer, bank, acnt, limit):
-	 ”””Create a new credit card instance.
+	 def __init__(self, customer, bank, acnt, limit):
+		 '''Create a new credit card instance.
 
-	 The initial balance is zero.
+		 The initial balance is zero.
 
-	 customer the name of the customer (e.g., John Bowman )
-	 bank the name of the bank (e.g., California Savings )
-	 acnt the acount identifier (e.g., 5391 0375 9387 5309 )
-	 limit credit limit (measured in dollars)
-	 ”””
-	 self. customer = customer
-	 self. bank = bank
-	 self. account = acnt
-	 self. limit = limit
-	 self. balance = 0
+		 customer the name of the customer (e.g., John Bowman )
+		 bank the name of the bank (e.g., California Savings )
+		 acnt the acount identifier (e.g., 5391 0375 9387 5309 )
+		 limit credit limit (measured in dollars) '''
+		 
+		 self._customer = customer
+		 self._bank = bank
+		 self._account = acnt
+		 self._limit = limit
+		 self._balance = 0
 
-	 def get customer(self):
-	 ”””Return name of the customer.”””
-	 return self. customer
+	 def get_customer(self):
+		 '''Return name of the customer.'''
+		 return self._customer
 
-	 def get bank(self):
-	 ”””Return the bank s name.”””
-	 return self. bank
+	 def get_bank(self):
+		 '''Return the bank s name.'''
+		 return self._bank
 
-	 def get account(self):
-	 ”””Return the card identifying number (typically stored as a string).”””
-	 return self. account
+	 def get_account(self):
+		 '''Return the card identifying number (typically stored as a string).'''
+		 return self._account
 
-	 def get limit(self):
-	 ”””Return current credit limit.”””
-	 return self. limit
+	 def get_limit(self):
+		 '''Return current credit limit.'''
+		 return self._limit
 
-	 def get balance(self):
-	 ”””Return current balance.”””
-	 return self. balance
+	 def get_balance(self):
+		 '''Return current balance.'''
+		 return self._balance
 	 def charge(self, price):
-	 ”””Charge given price to the card, assuming sufficient credit limit.
+		 '''Charge given price to the card, assuming sufficient credit limit.
 
-	 Return True if charge was processed; False if charge was denied.
-	 ”””
-	 if price + self. balance > self. limit: # if charge would exceed limit,
-	 return False # cannot accept charge
-	 else:
-	 self. balance += price
-	 return True
+		 Return True if charge was processed; False if charge was denied.
+		 '''
+		 if price + self._balance > self._limit: # if charge would exceed limit,
+			return False # cannot accept charge
+		 else:
+			self._balance += price
+			return True
 
-	 def make payment(self, amount):
-	 ”””Process customer payment that reduces balance.”””
-	 self. balance -= amount
+	 def make_payment(self, amount):
+		 '''Process customer payment that reduces balance.'''
+		 self._balance -= amount
