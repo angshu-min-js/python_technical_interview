3f33e1c3b2d9763f7eca426c7ee54216f23d70d6
angshu-min-js <angshu.gupta789@gmail.com>
2016-10-10 19:24:40 +0530

    code clean

--- 2.2_Vector.py
+++ 2.2_Vector.py
@@ -1,39 +1,39 @@
 class Vector:
-	 ”””Represent a vector in a multidimensional space.”””
+	 '''Represent a vector in a multidimensional space.'''
 
-	 def init (self, d):
-	 ”””Create d-dimensional vector of zeros.”””
-	 self. coords = [0] d
+	 def __init__(self, d):
+		 '''Create d-dimensional vector of zeros.'''
+		 self._coords = [0]*d
 
-	 def len (self):
-	 ”””Return the dimension of the vector.”””
-	 return len(self. coords)
+	 def __len__(self):
+		 '''Return the dimension of the vector.'''
+		 return len(self._coords)
 
-	 def getitem (self, j):
-	 ”””Return jth coordinate of vector.”””
-	 return self. coords[j]
+	 def __getitem__(self, j):
+		 '''Return jth coordinate of vector.'''
+		 return self._coords[j]
 
-	 def setitem (self, j, val):
-	 ”””Set jth coordinate of vector to given value.”””
-	 self. coords[j] = val
+	 def __setitem__(self, j, val):
+		 '''Set jth coordinate of vector to given value.'''
+		 self._coords[j] = val
 
-	 def add (self, other):
-	 ”””Return sum of two vectors.”””
-	 if len(self) != len(other): # relies on len method
-	 raise ValueError( dimensions must agree )
-	 result = Vector(len(self)) # start with vector of zeros
-	 for j in range(len(self)):
-	 result[j] = self[j] + other[j]
-	 return result
+	 def __add__(self, other):
+		 '''Return sum of two vectors.'''
+		 if len(self) != len(other): # relies on len method
+			 raise ValueError( "dimensions must agree" )
+		 result = Vector(len(self)) # start with vector of zeros
+		 for j in range(len(self)):
+			result[j] = self[j] + other[j]
+		 return result
 
-	 def eq (self, other):
-	 ”””Return True if vector has same coordinates as other.”””
-	 return self. coords == other. coords
+	 def __eq__(self, other):
+		 '''Return True if vector has same coordinates as other.'''
+		 return self._coords == other._coords
 
-	 def ne (self, other):
-	 ”””Return True if vector differs from other.”””
-	 return not self == other # rely on existing eq definition
+	 def __ne__(self, other):
+		 '''Return True if vector differs from other.'''
+		 return not self == other # rely on existing eq definition
 
-	 def str (self):
-	 ”””Produce string representation of vector.”””
-	 return < + str(self. coords)[1:-1] + > # adapt list representation
+	 def __str__(self):
+		 '''Produce string representation of vector.'''
+		 return < + str(self._coords)[1:-1] + > # adapt list representation
