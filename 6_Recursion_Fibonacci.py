f479c37f8d9f4b212ed1c9feaf4160cada13eba5
angshu-min-js <angshu.gupta789@gmail.com>
2016-10-14 12:42:45 +0530

    update

---
+++ 6_Recursion_Fibonacci.py
@@ -0,0 +1,10 @@
+def get_fib(position):
+    if position == 0 or position == 1:
+        return position
+    return get_fib(position - 1) + get_fib(position - 2)
+
+
+# Test cases
+print get_fib(9)
+print get_fib(11)
+print get_fib(0)
